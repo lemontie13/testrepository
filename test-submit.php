30760e1c5329e17c29f95c8ebb0943f5eb0d9f0f
lemontie13 <lemontie13@gmail.com>
2014-07-10 12:42:51 -0700

    Update test-submit.php

--- test-submit.php
+++ test-submit.php
@@ -9,12 +9,14 @@
 
 <?php
 	if(isset($_GET["device"]) && isset($_GET["carrier"])) {
-		echo "Things are here";
+
 
 		$device = $_GET["device"];
 		$carrier = $_GET["carrier"];
 
-		$server = "tcp:<mxx6aa5ssr>";
+		echo $device + " " + $carrier;
+
+		/*$server = "tcp:<mxx6aa5ssr>";
 		$user = "<value of USERNAME from section above>"@SERVER_ID;
 		$pwd = "my-test-db";
 		$db = "testdb";
@@ -32,7 +34,7 @@
 			<p><?= $row["Device"] + " " + $row["Customer Name"] + " " + $row["OS"] + " " ?> </p>
 		<?php
 
-		}
+		}*/
 	}
 ?>
 
