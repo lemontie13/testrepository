7801237c7dd8f4898f3efab774675c8461640c1d
lemontie13 <lemontie13@gmail.com>
2014-07-10 12:37:54 -0700

    Update test-submit.php

--- test-submit.php
+++ test-submit.php
@@ -1,6 +1,26 @@
 <?php
 	if(isset($_GET["device"]) && isset($_GET["carrier"])) {
-		echo $_GET["device"];
-		echo $_GET["carrier"];
+		$device = $_GET["device"];
+		$carrier = $_GET["carrier"];
+
+		$server = "tcp:<mxx6aa5ssr>";
+		$user = "<value of USERNAME from section above>"@SERVER_ID;
+		$pwd = "my-test-db";
+		$db = "testdb";
+
+		try {
+    		$conn = new PDO( "sqlsrv:Server= $server ; Database = $db ", $user, $pwd);
+    		$conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+		} catch(Exception $e) {
+    		die(print_r($e));
+		}
+
+		$rows = $conn->query("SELECT * FROM [Devices] WHERE Device = $device AND Carrier = $carrier ORDER BY Date DESC");
+		foreach($rows as $row) {
+		?>
+			<p><?= $row["Device"] + " " + $row["Customer Name"] + " " + $row["OS"] + " " ?> </p>
+		<?php
+		
+		}
 	}
 ?>
