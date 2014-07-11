b00fc74d758d41f0ce5140af203dfcf95f11f30d
lemontie13 <lemontie13@gmail.com>
2014-07-10 17:03:31 -0700

    Create add-submit.php

---
+++ add-submit.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Add Submit Test</title>
+		
+	</head>
+
+	<body>
+		<p>"Added"</p>
+<!--<?php
+	if(isset($_GET["device"]) && isset($_GET["carrier"])) {
+
+		$device = $_GET["device"];
+		$carrier = $_GET["carrier"];
+
+		$server = "tcp:mxx6aa5ssr.database.windows.net,1433";
+		$user = "my-test-db@mxx6aa5ssr";
+		$pwd = "Happyness!";
+		$db = "testdb";
+
+		try {
+    		$conn = new PDO( "sqlsrv:server= $server ; Database = $db ", $user, $pwd);
+    		$conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+		} catch(Exception $e) {
+    		die(print_r($e));
+		}
+
+		$device = $conn->quote($device);
+		$carrier = $conn->quote($carrier);
+
+		$rows = $conn->query("SELECT * FROM devices WHERE device = $device AND carrier = $carrier ORDER BY release_date DESC");
+		
+
+		foreach($rows as $row) {
+			$device_name = $row["device"];
+			$customer_name = $row["customer_name"];
+			$os = $row["os"];
+			$os_version = $row["os_version"];
+			$sense = $row["sense_version"];
+			$sw_version = $row["software_version"];
+			$baseband = $row["baseband"];
+			$release = $row["release_date"];
+			$carrier_name = $row["carrier"];
+		?>
+			<p><?=$carrier_name?>'s <?=$device_name?> (<?=$customer_name?>) was updated to <?= $os . " " . $os_version?>, Sense <?=$sense?>, SW version <?=$sw_version?>, baseband <?=$baseband?> on <?=$release?></p>
+
+		<?php
+
+		}
+	}
+?>-->
+
+</body>
+</html>
